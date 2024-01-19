d0d2496a728102ad855f51739820a24410b269d6
Oliver Jehle <ojehle@yahoo.com>
2024-01-19 09:36:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,153 @@
+# Oracle Patch Downloader
+
+The Oracle Patch Downloader can be used to download a list of patches from oracle.
+
+## Precoditions
+
+- java >= 11
+- maven >= 3.9
+- Oracle Support Account
+
+## Installation
+
+```sh
+git clone git@github.com:ojehle/H35_OraclePatchDownloader.git
+cd H35_OraclePatchDownloader
+mvn install
+cd target
+## verfiy build, you can copy the file (its complete with all dependencies)
+java -jar OraclePatchDownloader-1.0.0.jar 
+```
+
+
+## Usage
+
+You can use the tool to download a list of patches, it accepts multiple -x or a list of patches delimited by comma.
+the regex can be used to filter out the files
+ 
+```sh
+ -h : --help        help text
+
+ -d : --directory   output folder, default user home
+
+ -x : --patch       comma separated list of patches or multiple possible
+
+ -f : --patchfile   patch list as file, one patch per line , line starting with # is ignored, format p12345678, 12345678
+
+ -t : --plattform   Plattform Code 226P (Linux X86_64) 
+
+ -r : --regex       regex for file filter, multiple possible
+
+ -u : --user        email/userid
+
+ -p : --password    password
+```
+
+```sh
+java -jar OraclePatchDownloader-1.0.0.jar -u user@h35.li -p password -x 200000 -t 226P -r  ".*1900.*" -r  ".*19190.*" -d $HOME/Downloads
+```
+
+
+
+## Plattform Codes (-t)
+
+| Code  | Plattform |
+| ----- | -------------------------------------|
+| 537P| Acme Packet 1100|  
+| 529P| Acme Packet 3820|
+| 540P| Acme Packet 3900|
+| 561P| Acme Packet 3950|
+| 530P| Acme Packet 4500|
+| 538P| Acme Packet 4600|
+| 560P| Acme Packet 4900|
+| 534P| Acme Packet 6100|
+| 531P| Acme Packet 6300|
+| 551P| Acme Packet 6350|
+| 527P| Acme Packet OS|
+| 512P| Apple iOS|
+| 293P| Apple Mac OS X (Intel) (32-bit)|
+| 522P| Apple Mac OS X (Intel) (64-bit)|
+| 421P| Apple Mac OS X (PowerPC)|
+| 553P| Apple macOS|
+| 559P| Apple macOS ARM (64-bit)|
+| 516P| Embedded Linux ARM|
+| 521P| Embedded Linux on cnMIPS|
+| 519P| Embedded Linux on POWER e500v2|
+| 517P| Embedded Linux on POWER Systems|
+| 294P| Embedded Linux SH4|
+| 515P| Embedded Linux x86|
+| 228P| FreeBSD - x86|
+| 361P| Fujitsu BS2000|
+| 312P| Fujitsu BS2000/OSD (SQ series)|
+| 285P| Fujitsu BS2000/OSD (SX series)|
+| 504P| Fujitsu MSP-EX|
+| 513P| Google Android|
+| 554P| Google Android 9|
+| 549P| HP NonStop (Guardian) on x86|
+| 548P| HP NonStop (OSS) on x86|
+| 304P| HP NonStop Itanium (Guardian)|
+| 308P| HP NonStop Itanium (OSS)|
+| 303P| HP NonStop S-series (Guardian)|
+| 89P| HP OpenVMS Alpha|
+| 243P| HP OpenVMS Itanium|
+| 1P| HP OpenVMS VAX|
+| 87P| HP Tru64 UNIX|
+| 197P| HP-UX Itanium|
+| 278P| HP-UX Itanium (32-bit)|
+| 999P| HP-UX PA-RISC (32-bit)|
+| 59P| HP-UX PA-RISC (64-bit)|
+| 299P| ia64|
+| 319P| IBM AIX on POWER Systems (32-bit)|
+| 212P| IBM AIX on POWER Systems (64-bit)|
+| 43P| IBM i on POWER Systems|
+| 211P| IBM S/390 Based Linux (31-bit)|
+| 30P| IBM z/OS on System z|
+| 314P| IBM z/VM on System z|
+| 227P| IBM: Linux on POWER Big Endian Systems|
+| 542P| IBM: Linux on POWER Little Endian Systems|
+| 209P| IBM: Linux on System z|
+| 297P| iTron|
+| 528P| Linux ARM 32-bit VFP HardFP ABI|
+| 523P| Linux ARM 32-bit VFP SoftFP ABI|
+| 564P| Linux ARM 64 bit (DB19 OL79)|
+| 541P| Linux ARM 64-bit|
+| 214P| Linux Itanium|
+| 556P| Linux MIPS 64-bit|
+| 525P| Linux SPARC|
+| 46P| Linux x86|
+| 226P| Linux x86-64|
+| 99999P| Metadata-Only Patch|
+| 912P| Microsoft Windows (32-bit)|
+| 558P| Microsoft Windows AArch64|
+| 208P| Microsoft Windows Itanium (64-bit)|
+| 539P| Microsoft Windows Phone|
+| 233P| Microsoft Windows x64 (64-bit)|
+| 276P| Monta Vista x86|
+| 277P| Monta Vista x86-64|
+| 536P| Net-Net 4250|
+| 533P| Net-Net 9200|
+| 547P| Netra Server X5-2 for Communications|
+| 535P| Netra X3-2 for Acme Packet|
+| 1234P| NLS Generic Platform|
+| 2234P| NLS Merged Translations|
+| 3234P| NLS Pseudo Translation|
+| 313P| OpenSolaris|
+| 309P| Oracle JRockit Virtual Edition x86 (32-bit)|
+| 311P| Oracle JRockit Virtual Edition x86-64 (64-bit)|
+| 506P| Oracle Solaris Express|
+| 453P| Oracle Solaris on SPARC (32-bit)|
+| 23P| Oracle Solaris on SPARC (64-bit)|
+| 173P| Oracle Solaris on x86 (32-bit)|
+| 267P| Oracle Solaris on x86-64 (64-bit)|
+| 511P| QNX UNIX|
+| 28P| SCO UNIX|
+| 505P| SPARC|
+| 550P| StorageTek Hardware|
+| 619P| Stratus PA-RISC VOS|
+| 26P| Symbian EPOC|
+| 555P| Talari|
+| 532P| Tekelec|
+| 316P| Unisys OS 2200|
+| 520P| VxWorks|
+| 280P| x86 32 bit|
+| 282P| x86 64 bit|
---
+++ src/main/java/li/h35/OraclePatchDownloader/OraclePatchDownloader.java
@@ -0,0 +1,294 @@
+package li.h35.OraclePatchDownloader;
+
+/*
+ * Copyright (c) 2024 H35 GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.htmlunit.BrowserVersion;
+import org.htmlunit.Page;
+import org.htmlunit.UnexpectedPage;
+import org.htmlunit.WebClient;
+import org.htmlunit.html.DomElement;
+import org.htmlunit.html.HtmlForm;
+import org.htmlunit.html.HtmlInput;
+import org.htmlunit.html.HtmlPage;
+import org.htmlunit.html.HtmlTable;
+import org.htmlunit.html.HtmlTableCell;
+import org.htmlunit.html.HtmlTableRow;
+
+import gnu.getopt.Getopt;
+import gnu.getopt.LongOpt;
+
+public class OraclePatchDownloader {
+	private static String patchRegex = "^([p]{0,1})([0-9]{8}).*$";
+	private static Pattern patchPattern = Pattern.compile(patchRegex);
+	private static String regex = "(?:^|\\?|&)patch_file=(.*?)(?:&|$)";
+	private static Pattern pattern = Pattern.compile(regex);
+	private static File directory = null;
+	private static String plattform = null;
+	private static String user = null;
+	private static String password = null;
+	private static ArrayList<Pattern> patchRegexp = new ArrayList<Pattern>();
+	private static ArrayList<String> patchList = new ArrayList<String>();
+
+	public static String getPatchFile(String url) {
+		Matcher matcher = pattern.matcher(url);
+		if (matcher.find()) {
+			return matcher.group(1);
+		}
+		return "";
+	}
+
+	public String getDownloadUrl(String patch) {
+		return "https://updates.oracle.com/Orion/SimpleSearch/process_form?search_type=patch&patch_number=" + patch
+				+ "&plat_lang=" + plattform;
+	}
+
+	public boolean isPatchDownloaded(String patch) {
+		String[] list = directory.list(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.startsWith("p" + patch) && name.endsWith(".zip");
+			}
+		});
+		return list.length > 0;
+	}
+
+	public OraclePatchDownloader() {
+		ArrayList<String> downloads = new ArrayList<String>();
+		boolean loggedIn = false;
+
+		WebClient webClient = new WebClient(BrowserVersion.FIREFOX);
+		webClient.getOptions().setJavaScriptEnabled(true);
+		Logger.getLogger("org.htmlunit").setLevel(Level.SEVERE);
+		HtmlPage page = null;
+		try {
+
+			for (String patch : patchList) {
+				if (isPatchDownloaded(patch))
+					continue;
+
+				page = webClient.getPage(getDownloadUrl(patch));
+				if (!loggedIn) {
+					for (HtmlForm f : page.getForms()) {
+						if (f.getNameAttribute().equalsIgnoreCase("LoginForm")) {
+							HtmlForm form = page.getFormByName("LoginForm");
+							form.getInputByName("ssousername").type(user);
+							form.getInputByName("password").type(password);
+							HtmlInput in = page.getHtmlElementById("signin_button");
+							page = in.click(); // works fine
+							page = webClient.getPage(getDownloadUrl(patch));
+						}
+					}
+				}
+
+				for (DomElement e : page.getElementsByTagName("table")) {
+					if (e instanceof HtmlTable) {
+						for (HtmlTableRow r : ((HtmlTable) e).getRows()) {
+							for (HtmlTableCell c : r.getCells()) {
+								for (org.htmlunit.html.HtmlElement el : c.getHtmlElementDescendants()) {
+									String u = el.getAttribute("href");
+									if (u.startsWith("https") && u.contains(".zip")) {
+										if (patchRegexp.size() > 0) {
+											for (Pattern p : patchRegexp) {
+												if (p.matcher(u).matches()) {
+													downloads.add(u);
+													break;
+												}
+											}
+
+										} else {
+											downloads.add(u);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			for (String u : downloads) {
+				String filename = getPatchFile(u);
+				File outputFile = new File(directory, filename);
+				if (outputFile.exists() && outputFile.length() > 0)
+					continue;
+
+				Page p = webClient.getPage(u);
+				if (p.isHtmlPage())
+					continue;
+				UnexpectedPage unexpectedPage = (UnexpectedPage) p;
+				InputStream inputStream = unexpectedPage.getInputStream();
+				// Save the stream to the file
+				FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+				byte[] buffer = new byte[8192];
+				int bytesRead;
+				while ((bytesRead = inputStream.read(buffer)) != -1) {
+					outputStream.write(buffer, 0, bytesRead);
+				}
+				System.out.println("File " + filename + " downloaded successfully.");
+				outputStream.close();
+			}
+			webClient.close();
+			int exitRc = 0;
+			for (String patch : patchList) {
+
+				if (isPatchDownloaded(patch))
+					continue;
+				exitRc = 1;
+				System.out.println("Patch " + patch + " missing");
+			}
+			System.exit(exitRc);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	private static void help() {
+		System.out.println("usage:");
+		System.out.println(" -h : --help        help text");
+		System.out.println(" -d : --directory   output folder, default user home");
+		System.out.println(" -x : --patch       comma separated list of patches or multiple possible");
+		System.out.println(" -f : --patchfile   patch list as file, one patch per line , # is ignored");
+		System.out.println(" -t : --plattform   Plattform Code 226P (Linux X86_64) ");
+		System.out.println(" -r : --regex       regex for file filter, multiple possible");
+		System.out.println(" -u : --user        email/userid");
+		System.out.println(" -p : --password    password");
+	}
+
+	public static void main(String[] args) {
+
+		int c;
+		LongOpt[] longopts = new LongOpt[8];
+		longopts[0] = new LongOpt("help", LongOpt.NO_ARGUMENT, null, 'h');
+		longopts[1] = new LongOpt("directory", LongOpt.REQUIRED_ARGUMENT, null, 'd');
+		longopts[2] = new LongOpt("patches", LongOpt.REQUIRED_ARGUMENT, null, 'x');
+		longopts[3] = new LongOpt("plattform", LongOpt.REQUIRED_ARGUMENT, null, 't');
+		longopts[4] = new LongOpt("regex", LongOpt.REQUIRED_ARGUMENT, null, 'r');
+		longopts[5] = new LongOpt("user", LongOpt.REQUIRED_ARGUMENT, null, 'u');
+		longopts[6] = new LongOpt("password", LongOpt.REQUIRED_ARGUMENT, null, 'p');
+		longopts[7] = new LongOpt("patchfile", LongOpt.REQUIRED_ARGUMENT, null, 'f');
+
+		Getopt g = new Getopt("OraclePatchDownoader", args, "hd:t:x:r:u:p:f:", longopts);
+		directory = new File(System.getProperty("user.home"));
+
+		while ((c = g.getopt()) != -1)
+			switch (c) {
+			case 'h':
+				System.out.println("usage:");
+				System.exit(0);
+				break;
+
+			case 'd':
+				directory = new File(g.getOptarg());
+
+				if (!directory.exists()) {
+					if (directory.getParentFile().exists())
+						directory.mkdir();
+				}
+				break;
+
+			//
+			case 'r':
+				Pattern p = null;
+				String s = g.getOptarg();
+				try {
+					p = Pattern.compile(s);
+				} catch (Exception e) {
+					System.err.println("cannot parse regexp : " + s);
+					e.printStackTrace();
+					System.exit(1);
+				}
+				if (p != null)
+					patchRegexp.add(p);
+				break;
+
+			case 'x':
+				String[] pl = g.getOptarg().split(",");
+				for (String patch : pl) {
+					patchList.add(patch);
+				}
+				break;
+
+			case 'f':
+				File fp = new File(g.getOptarg());
+				if (fp.exists()) {
+					String line;
+					try {
+					BufferedReader br = new BufferedReader(new FileReader(fp));
+					while ((line = br.readLine()) != null) {
+						Matcher matcher = patchPattern.matcher(line.trim());
+						if (matcher.matches()) {
+							String px = matcher.group(2);
+							patchList.add(px);
+						}
+					}
+					br.close();
+					} catch(Exception e) {
+						e.printStackTrace();
+						System.exit(1);
+					}			
+				}
+				break;
+
+
+			case 't':
+				plattform = g.getOptarg();
+				break;
+			case 'p':
+				password = g.getOptarg();
+				break;
+			case 'u':
+				user = g.getOptarg();
+				break;
+
+			default:
+				help();
+				System.exit(1);
+				break;
+			}
+
+		if (user == null || password == null) {
+			System.err.println("User or Password missing");
+			help();
+			System.exit(1);
+		}
+		if (plattform == null) {
+			System.err.println("Plattform missing");
+			help();
+			System.exit(1);
+		}
+		if (patchList.size() == 0) {
+			System.err.println("no patches specified");
+			help();
+			System.exit(1);
+		}
+
+		new OraclePatchDownloader();
+
+	}
+}
